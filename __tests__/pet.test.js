22bbd9b61fe7d0c5b0dcec4307aef7fe0b0bbb48
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-09 16:07:31 +0100

    added hunger and fitness property to constructor function and growUp method alters these values

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,32 +2,68 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-  });
+});
 
-  describe('constructor', () => {
+describe('constructor', () => {
     it('sets the name property', () => {
-      const pet = new Pet('Fido');
+        const pet = new Pet('Fido');
   
-      expect(pet.name).toEqual('Fido');
+        expect(pet.name).toEqual('Fido');
     });
-  });
+});
 
-  describe('constructor', () => {
-      it('has a initial age of 0', () =>{
-          const pet = new Pet('Fido');
+describe('constructor', () => {
+    it('has a initial age of 0', () =>{
+        const pet = new Pet('Fido');
 
-          expect(pet.age).toEqual(0);
-      });
-  });
+        expect(pet.age).toEqual(0);
+    });
+});
 
-  describe('constructor', () => {
+describe('constructor', () => {
     it('increments the age by 1', () =>{
         const pet = new Pet('Fido');
 
         pet.growUp();
 
         expect(pet.age).toEqual(1);
+    });
+});
+
+describe('constructor', () => {
+    it('has a initial hunger of 0', () =>{
+        const pet = new Pet('Fido');
+
+        expect(pet.hunger).toEqual(0);
+    });
+});
+
+describe('constructor', () => {
+    it('increments the hunger by 5', () =>{
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+});
+
+describe('constructor', () => {
+    it('has a initial fitness of 10', () =>{
+        const pet = new Pet('Fido');
+
+        expect(pet.fitness).toEqual(10);
+    });
+});
+
+describe('constructor', () => {
+    it('decreases the fitness by 3', () =>{
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,14 @@
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
 
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
 };
 
 module.exports = Pet;
